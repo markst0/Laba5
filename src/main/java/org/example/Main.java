0a85ef76ab60a7e07d7172df633bf1a568f2cacc
markst0 <133260129+markst0@users.noreply.github.com>
2024-12-23 20:26:20 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,17 @@
 package org.example;
 
+/**
+ * Главный класс для запуска приложения.
+ * Демонстрирует использование {@link Injector} для автоматического внедрения зависимостей
+ */
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Injector injector = new Injector("src/main/resources/config.properties");
+            OrderService orderService = injector.inject(new OrderService());
+            orderService.placeOrder(500.0, "Payment successful for your order!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
