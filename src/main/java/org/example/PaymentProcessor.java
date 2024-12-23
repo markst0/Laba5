58b63ab76f90f181de7a3b458dcdff52c862ece4
markst0 <133260129+markst0@users.noreply.github.com>
2024-12-23 20:26:57 +0300

    Update PaymentProcessor.java

--- src/main/java/org/example/PaymentProcessor.java
+++ src/main/java/org/example/PaymentProcessor.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public class PaymentProcessor {
+/**
+ * Интерфейс для обработки платежей
+ */
+public interface PaymentProcessor {
+    /**
+     * Обрабатывает платеж
+     * @param amount сумма для оплаты
+     */
+    void processPayment(double amount);
 }
